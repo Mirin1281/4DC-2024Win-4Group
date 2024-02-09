890d95b6ddbb9579a7f9cb7bf0d262d1d7a410c9
Mirin <113825778+Mirin1281@users.noreply.github.com>
2024-02-09 11:27:59 +0900

    ストーリーにBGMつけた

--- Assets/Plugins/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ Assets/Plugins/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -9,6 +9,7 @@ public static class BGMPath{
 	public const string CLUB_F          = "BGM/club_f";
 	public const string FOURDC302       = "BGM/Fourdc3-02";
 	public const string KARI_KISEION_01 = "BGM/kari_kiseion_01";
+	public const string STORY           = "BGM/story";
 
 }
 
