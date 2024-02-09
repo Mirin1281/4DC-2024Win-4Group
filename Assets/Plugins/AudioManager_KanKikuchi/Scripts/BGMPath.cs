3e6046548f16499a54636a04657c5fe71e8a95ae
Mirin <113825778+Mirin1281@users.noreply.github.com>
2024-02-09 09:39:11 +0900

    タイトル差し替えとBGM追加

--- Assets/Plugins/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ Assets/Plugins/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -5,6 +5,7 @@
 /// </summary>
 public static class BGMPath{
 
+	public const string CHILL_MORNING_F = "BGM/chill_morning_f";
 	public const string CLUB_F          = "BGM/club_f";
 	public const string FOURDC302       = "BGM/Fourdc3-02";
 	public const string KARI_KISEION_01 = "BGM/kari_kiseion_01";
