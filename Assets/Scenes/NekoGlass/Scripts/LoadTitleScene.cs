354bbd083fdd998ea664304ca459de9a38fb93f2
BattleNuko <srcolorsplash@gmail.com>
2024-02-09 13:36:25 +0900

    本番さいしゅう

--- Assets/Scenes/NekoGlass/Scripts/LoadTitleScene.cs
+++ Assets/Scenes/NekoGlass/Scripts/LoadTitleScene.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// LoadTitleSceneとか言いつつMainSceneへも移動するスクリプト
+/// </summary>
+
 public class LoadTitleScene : MonoBehaviour
 {
     public string TitleSceneName = "Title";
+    public string MainSceneName = "MainScene";
 
     public void LoadTitle()
     {
         SceneManager.LoadScene(TitleSceneName);
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(MainSceneName);
     }
 }
